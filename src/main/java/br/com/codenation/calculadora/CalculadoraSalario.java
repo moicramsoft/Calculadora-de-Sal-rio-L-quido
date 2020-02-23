4a90a228e3b659c4408464aa42c2f15358707088
MoicraSilva <moicramsilva@gmail.com>
2020-02-22 22:57:51 -0300

    V3

--- src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
+++ src/main/java/br/com/codenation/calculadora/CalculadoraSalario.java
@@ -2,44 +2,86 @@ package br.com.codenation.calculadora;
 
 
 public class CalculadoraSalario {
+	
 
-          private  int IRRF;
-          private  int INSS ;
-         private   int ateMil;
-         private final   double ateMilEQuinhentos=8;// descont de 8%
-         private final   int ateMilEQuinhentosUm=11;// descont de 11%
-         private final   int ateDezMil=11;// descont de 11%
-         private final   int isentoAteTresMil=0;
-         private final   int TresMilAteSeisMil=(int) 7.5;//7.5%
-         private   int AcimaDeSeisMil=15;//15%
-         private double desconto;
-            //salarioBase=3.000;
 	public long calcularSalarioLiquido(double salarioBase) {
-            
-		//Use o Math.round apenas no final do m√©todo para arredondar o valor final.
-		//Documenta√ß√£o do m√©todo: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
-		return Math.round(0.0);
-	}
-	
-	
-        //Exemplo de m√©t/*o*/do que pode ser criado para separar melhor as respons√°bilidades de seu algor√≠tmo
-	private double calcularInss(double salarioBase) {
-       if (salarioBase <= 1500) {
-            this.desconto = this.ateMilEQuinhentos;
-       } else {
-			this.desconto = 11d;
+		//Use o Math.round apenas no final do mÈtodo para arredondar o valor final.
+		//DocumentaÁ„o do mÈtodo: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html#round-double-
+                    var Variaveis= new var();
+                    
+		Variaveis.salarioBase = Variaveis.salarioBase;
+		if (Variaveis.salarioBase < Variaveis.vlSalarioMinimo) {
+			return 0;
 		}
-            
-		//return salarioBase - (salarioBase * (descInss + descIr) / 100); 
-            return 0;
+		Variaveis.calcularInss(Variaveis.salarioBase);
+		Variaveis.calcularIRRF(Variaveis.salarioBase);
+		Variaveis.salarioliquido = Variaveis.salarioBase - Variaveis.totalIRRF - Variaveis.totalINSS;
+		return Math.round(Variaveis.salarioliquido);
 	}
-   public static void main(String[] args) {
-           // CalculadoraSalario CalculadoraSalario = new CalculadoraSalario();
-           // CalculadoraSalario.calcularInss(1.000);
-           
-          
-    }
+
+
+	//Exemplo de mÈtodo que pode ser criado para separar melhor as respons·bilidades de seu algorÌtmo
+	public long calcularInss(double salarioBase) {
+		/**
+		 *
+		 Faixa salarial	Percentual de desconto
+		 AtÈ R$ 1.500,00	8%
+		 De R$ 1.500,01 atÈ R$ 4.000,00	9%
+		 Acima de R$ 4.000,00	11%
+		 */
+		double aliquota;
+                var Variaveis= new var();
+		if (Variaveis.salarioBase <= 1500) {
+			aliquota = 8d;
+		} else if (Variaveis.salarioBase >= 1500.01 && Variaveis.salarioBase <= 4000) {
+			aliquota = 9d;
+		} else {
+			aliquota = 11d;
+		}
+		Variaveis.totalINSS = Variaveis.salarioBase * aliquota / 100;
+		Variaveis.baseCalculoIRRF = Variaveis.salarioBase - Variaveis.totalINSS;
+		return Math.round(Variaveis.totalINSS);
+	}
+
+	/**
+	* @param salarioBase
+     * @return 
+	 */
+	public long calcularIRRF(double salarioBase) {
+		calcularInss(salarioBase);
+           	double aliquota = 0d;
+                var Variaveis= new var();
+		if (Variaveis.baseCalculoIRRF <= 3000) {
+			Variaveis.totalIRRF = 0d;
+		} else if (Variaveis.baseCalculoIRRF >= 3000.01 && Variaveis.baseCalculoIRRF <= 6000) {
+			aliquota = 7.5;
+		} else {
+			aliquota = 15d;
+		}
+		if (aliquota > 0)
+			Variaveis.totalIRRF = Variaveis.baseCalculoIRRF * aliquota / 100;
+
+		return Math.round(Variaveis.totalIRRF);
+		//aplicarDeducoes();
+	}
+
+	/**
+	 * AtÈ 1.903,98	0%	0,00
+	 * De 1.903,99 atÈ 2.826,65	7,5%	142,80
+	 * De 2.826,66 atÈ 3.751,05	15%	354,80
+	 * De 3.751,06 atÈ 4.664,68	22,5%	636,13
+	 * Acima de 4.664,69	27,5%	869,36
+	 */
+	private void aplicarDeducoes() {
+             var Variaveis= new var();
+		if (Variaveis.salarioBase >= 3000.0 && Variaveis.salarioBase <= 6000) {
+			Variaveis.totalIRRF -= 142.80;
+		} else if (Variaveis.salarioBase >= 6000.00)
+			Variaveis.totalIRRF -= 354.80;
+	}
 }
-/*D√∫vidas ou Problemas?
+
+
+/*D˙vidas ou Problemas?
 Manda e-mail para o meajuda@codenation.dev que iremos te ajudar! 
 */
